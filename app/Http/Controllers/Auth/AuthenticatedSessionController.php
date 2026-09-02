5324bb08782e0f153da184c3bab43588c7d8880f
nashwa aldebaran <nashwaaldebaran@gmail.com>
2026-09-02 19:43:12 +0700

    Update dashboard admin and login redirect

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -23,13 +23,20 @@ class AuthenticatedSessionController extends Controller
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
+{
+    $request->authenticate();
 
-        $request->session()->regenerate();
+    $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
-    }
+    $user = Auth::user();
+
+    return match ($user->role) {
+        'admin' => redirect('/admin'),
+        'petugas' => redirect('/petugas'),
+        'pengguna' => redirect('/pengguna'),
+        default => redirect('/'),
+    };
+}
 
     /**
      * Destroy an authenticated session.

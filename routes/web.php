f5d6009f223fb27e7ace7af776d3172983f2652c
anggitakirana <anggitakiranapuspa2005@gmail.com>
2026-09-13 23:42:57 +0700

    feat: add daftar reservasi petugas route

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,10 @@ Route::get('/petugas/reservasi/dashboard', function () {
     return view('petugas.reservasi.dashboard');
 })->name('petugas.reservasi.dashboard');
 
+Route::get('/petugas/reservasi/daftar-reservasi', function () {
+    return view('petugas.reservasi.daftar-reservasi');
+})->name('petugas.reservasi.index');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
